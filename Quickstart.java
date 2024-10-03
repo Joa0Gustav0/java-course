99af2eaeae84420c14b6efcaae0b91b2b0d0df76
Joao <dev.joaogustavo.dev@gmail.com>
2024-10-02 21:13:56 -0300

    first commit | quickstart

---
+++ Quickstart.java
@@ -0,0 +1,9 @@
+//Every Java file begins with a class.
+//This class' name always matchs the file name.
+class   Quickstart {
+  public static void main(String[] args) {
+    System.out.println("Hello, World! This is a quickstart.");
+    System.out.print("The multiplication for 2 and 5 is: ");
+    System.out.print(2 * 5 + "!!!");
+  }
+}
